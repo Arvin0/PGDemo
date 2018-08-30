33d870058b2dd48e617edb42ef290d631e6c85b7
zhanglonglong <zhang.longlong@synyi.com>
2018-08-30 18:31:27 +0800

    :bug:修改误操作

--- PGDemo.ApiCore/Extensions/MiddlewareExtensions.cs
+++ PGDemo.ApiCore/Extensions/MiddlewareExtensions.cs
@@ -3,11 +3,16 @@ using PGDemo.ApiCore.Middleware;
 
 namespace PGDemo.ApiCore.Extensions
 {
-    public static class ExceptionHandleExtensions
+    public static class MiddlewareExtensions
     {
         public static IApplicationBuilder UseExceptionHandle(this IApplicationBuilder app)
         {
             return app.UseMiddleware<ExceptionHandleMiddleware>();
+        }
+
+        public static IApplicationBuilder StatisticExecutedTime(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<ExecutedTimeMiddleware>();
         }
     }
 }
---
+++ PGDemo.ApiCore/Middleware/ExecutedTimeMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace PGDemo.ApiCore.Middleware
+{
+    public class ExecutedTimeMiddleware
+    {
+        private readonly ILogger<ExecutedTimeMiddleware> _logger;
+        private readonly RequestDelegate _next;
+
+        public ExecutedTimeMiddleware(RequestDelegate next, ILogger<ExecutedTimeMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            await _next(context);
+
+            stopwatch.Stop();
+            var message = $"{context.Request.GetDisplayUrl()} Time : {stopwatch.ElapsedMilliseconds}";
+            Console.WriteLine(message);
+            _logger.LogInformation(message);
+        }
+
+    }
+}
